00b6a6fc6209ed421192fe9eb7aa55f9406f462e
psyvision <richardcaunt@gmail.com>
2017-12-18 20:25:12 +0000

    Changes for Azure

--- core/server/config/index.js
+++ core/server/config/index.js
@@ -35,6 +35,8 @@ _private.loadNconf = function loadNconf(options) {
     nconf.file('default-env', path.join(baseConfigPath, 'env', 'config.' + env + '.json'));
     nconf.file('defaults', path.join(baseConfigPath, 'defaults.json'));
 
+    nconf.set('server:port', process.env.PORT);
+
     /**
      * transform all relative paths to absolute paths
      * transform sqlite filename path for Ghost-CLI
